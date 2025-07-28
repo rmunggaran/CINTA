9d1ce2598acf73f0e45c2f66c2a6e2e8372fd9d5
rmunggaran <riksamunggaran252@gmail.com>
2025-07-28 10:33:10 +0700

    updae login admin

--- login/masuk.php
+++ login/masuk.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="<?= $setting['logo'] ?>" />
@@ -15,25 +14,25 @@
 	<link rel="stylesheet" type="text/css" href="../dist/bootstrap/css/util.css">
 	<link rel="stylesheet" type="text/css" href="../dist/bootstrap/css/main.css">
 	<!-- CSS Libraries -->
-  <link rel="stylesheet" href="../assets/modules/bootstrap-social/bootstrap-social.css">
-  <link rel="stylesheet" href="../assets/modules/izitoast/css/iziToast.min.css">
- <link href="login.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <!-- CSS Libraries -->
-  <link rel="stylesheet" href="../assets/modules/bootstrap-social/bootstrap-social.css">
-  <link rel="stylesheet" href="../assets/modules/izitoast/css/iziToast.min.css">
-   <script>
-    window.dataLayer = window.dataLayer || [];
+	<link rel="stylesheet" href="../assets/modules/bootstrap-social/bootstrap-social.css">
+	<link rel="stylesheet" href="../assets/modules/izitoast/css/iziToast.min.css">
+	<link href="login.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- CSS Libraries -->
+	<link rel="stylesheet" href="../assets/modules/bootstrap-social/bootstrap-social.css">
+	<link rel="stylesheet" href="../assets/modules/izitoast/css/iziToast.min.css">
+	<script>
+		window.dataLayer = window.dataLayer || [];
 
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
 
-    gtag('config', 'UA-94034622-3');
-  </script>
-<!------ Include the above in your HEAD tag ---------->
+		gtag('config', 'UA-94034622-3');
+	</script>
+	<!------ Include the above in your HEAD tag ---------->
 	<title>LOGIN &mdash; ADMIN <?= $setting['nama_sekolah'] ?> </title>
 	<link rel="shortcut icon" href="../<?= $setting['logo'] ?>" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -51,12 +50,32 @@
 
 		.logo {
 			position: absolute;
-			left: 20px;
-			top: 20px;
-			z-index: 2;
-			color: #000;
+			top: 25%;
+			left: 35%;
+			transform: translateX(-50%);
+			z-index: 3;
 			-webkit-filter: drop-shadow(5px 5px 5px #222);
 			filter: drop-shadow(5px 5px 5px #222);
+		}
+
+		.logo img {
+			width: auto;
+			height: 120px;
+			/* ubah sesuai keinginan */
+		}
+
+
+		.tagline {
+			position: absolute;
+			top: 55%;
+			left: 35%;
+			transform: translate(-50%, -50%);
+			z-index: 3;
+			color: #fff;
+			font-size: 65px;
+			font-weight: bold;
+			text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+			text-align: center;
 		}
 
 		.candy {
@@ -95,17 +114,17 @@
 				</a>
 			</div>
 			<div class='logo hidden-xs'><img src="../<?= $setting['logo'] ?>" height="75" /></div>
-			<div class='candy hidden-xs'>
-				
-			</div>
+			<div class="tagline hidden-xs">EASY MI CONDONG <br> KOTA TASIKMALAYA</div>
+
+			<div class='candy hidden-xs'></div>
 			<div class="login100-more" style="background-image: url('../dist/back.jpg');"></div>
 
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
 				<form id="form-login">
 					<span class="animated flipInX login100-form-title">
-						APLIKASI PPDB ONLINE					</span>
+						APLIKASI PPDB ONLINE </span>
 					<small class="animated flipInX p-b-50">
-						<?= $setting['nama_sekolah'] ?>					</small>
+						<?= $setting['nama_sekolah'] ?> </small>
 
 					<div class="wrap-input100 validate-input p-t-50" data-validate="Username is required">
 						<span class="label-input100">Username</span>
@@ -137,57 +156,58 @@
 
 		</div>
 	</div>
-<!-- Page Specific JS File -->
-  <script src="../assets/modules/izitoast/js/iziToast.min.js"></script>
+	<!-- Page Specific JS File -->
+	<script src="../assets/modules/izitoast/js/iziToast.min.js"></script>
 	<script src='../plugins/jQuery/jquery-3.2.1.min.js'></script>
 	<script src='../dist/bootstrap/js/bootstrap.min.js'></script>
 
 	<script src="../plugins/jQuery/main.js"></script>
 	<!-- General JS Scripts -->
-  <script src="../assets/modules/jquery.min.js"></script>
-  <!-- <script src="../assets/modules/popper.js"></script>
+	<script src="../assets/modules/jquery.min.js"></script>
+	<!-- <script src="../assets/modules/popper.js"></script>
   <script src="../assets/modules/tooltip.js"></script> -->
-  <script src="../assets/modules/bootstrap/js/bootstrap.min.js"></script>
-  <script src="../assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-  <script src="../assets/modules/moment.min.js"></script>
-  <script src="../assets/js/stisla.js"></script>
+	<script src="../assets/modules/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+	<script src="../assets/modules/moment.min.js"></script>
+	<script src="../assets/js/stisla.js"></script>
 
-  <!-- JS Libraies -->
+	<!-- JS Libraies -->
 
-  <!-- Page Specific JS File -->
-  <script src="../assets/modules/izitoast/js/iziToast.min.js"></script>
-  <!-- Template JS File -->
-  <script src="../assets/js/scripts.js"></script>
-  <script src="../assets/js/custom.js"></script>
+	<!-- Page Specific JS File -->
+	<script src="../assets/modules/izitoast/js/iziToast.min.js"></script>
+	<!-- Template JS File -->
+	<script src="../assets/js/scripts.js"></script>
+	<script src="../assets/js/custom.js"></script>
 
 </body>
 <script>
-    $('#form-login').submit(function(e) {
-      e.preventDefault();
-      $.ajax({
-        type: 'POST',
-        url: 'login_cek.php?id=5448dfhcr27467576c78a50vi98j0ruv0w',
-        data: $(this).serialize(),
-        success: function(data) {
-          if (data == "ok") {
-            iziToast.success({
-              title: 'Berhasil!',
-              message: 'Anda akan dialihkan',
-              position: 'topRight'
-            });
-            setTimeout(function() {
-              window.location.reload();
-            }, 2000);
-          } else {
-            iziToast.error({
-              title: 'Maaf !!',
-              message: 'Username atau Password Salah',
-              position: 'topCenter'
-            });
-          }
-        }
-      });
-      return false;
-    });
-  </script>
+	$('#form-login').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: 'login_cek.php?id=5448dfhcr27467576c78a50vi98j0ruv0w',
+			data: $(this).serialize(),
+			success: function(data) {
+				if (data == "ok") {
+					iziToast.success({
+						title: 'Berhasil!',
+						message: 'Anda akan dialihkan',
+						position: 'topRight'
+					});
+					setTimeout(function() {
+						window.location.reload();
+					}, 2000);
+				} else {
+					iziToast.error({
+						title: 'Maaf !!',
+						message: 'Username atau Password Salah',
+						position: 'topCenter'
+					});
+				}
+			}
+		});
+		return false;
+	});
+</script>
+
 </html>
